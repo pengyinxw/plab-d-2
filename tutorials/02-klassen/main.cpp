4d5bf9e4e742dd5d5122bb5cdaf1437a75408da5
IMS Programmier Projekt Gruppe 03 <plab03@pxe-91.ims.intern>
2017-11-07 17:28:30 +0100

    Tutorial 2 angefangen, Tutorial 3 fertig

--- tutorials/02-klassen/main.cpp
+++ tutorials/02-klassen/main.cpp
@@ -3,7 +3,62 @@
  *      Author: scharf
  */
 
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Fahrzeug {
+public:
+    std::string marke;
+    std::string farbe;
+    int fahrgestellnummer;
+    Fahrzeug::Fahrzeug(string m, string f, int fn) : marke(m), farbe(f), fahrgestellnummer(fn){}
+};
+
+class Parkplatz {
+    private: std::vector<Fahrzeug> fahrzeuge;
+};
+
+class MotorisiertesFahrzeug : public Fahrzeug {
+public :
+    MotorisiertesFahrzeug::MotorisiertesFahrzeug(string m, string f, int fn): Fahrzeug(m,f,fn){}
+    void fahren() {
+        std::cout << "Fahren!" << std::endl;
+    }
+};
+
+class NichtMotorisiertesFahrzeug : public Fahrzeug {
+public:
+    virtual void strampeln();
+    NichtMotorisiertesFahrzeug::NichtMotorisiertesFahrzeug(string m, string f, int fn): Fahrzeug(m,f,fn){}
+};
+
+class LKW : public MotorisiertesFahrzeug {
+public : LKW::LKW(string m, string f, int fn): MotorisiertesFahrzeug(m,f,fn){}
+};
+
+class PKW : public MotorisiertesFahrzeug {
+public : PKW::PKW(string m, string f, int fn): MotorisiertesFahrzeug(m,f,fn){}
+};
+
+class Motorrad : public MotorisiertesFahrzeug {
+public : Motorrad::Motorrad(string m, string f, int fn): MotorisiertesFahrzeug(m,f,fn){}
+};
+
+class Fahrrad : NichtMotorisiertesFahrzeug {
+public : Fahrrad::Fahrrad(string m, string f, int fn): NichtMotorisiertesFahrzeug(m,f,fn){}
+    void strampeln() override{
+        std::cout << "Strampeln!" << std::endl;
+    }
+};
+
+
 int main(void) {
   //create some test objects here
+
+    Fahrrad rad1("Schrotteil","rost", 123);
+    rad1.strampeln();
 	return 0;
 }
--- tutorials/03-api/Tutorial.cpp
+++ tutorials/03-api/Tutorial.cpp
@@ -33,8 +33,28 @@ void Tutorial::run() {
 
 	Parser::parse(argv.size(), &argv[0], graphHandler.get());
 
-	//insert code here
+    //insert code here
+    std::cout << graphHandler->getElement(5)->getName() << std::endl << "outgoing: \n";
+    std::vector<const Net*> outNets = graphHandler->getElement(5)->getOutNets();
+    for (const Net* n:outNets)
+        std::cout << n->getId() << " , " << n->getName() << std::endl;
+    std::cout<<"incoming: \n";
+    std::vector<const Net*> inNets = graphHandler->getElement(5)->getInNets();
+    for (const Net* n:inNets)
+        std::cout << n->getId() << " , " << n->getName() << std::endl;
 
+    std::cout<<"Alle Elemente:"<<std::endl;
+    std::vector<const Element*> elems = graphHandler->getAllElements();
+    for (const Element* e:elems)
+        std::cout << e->getId() << " , " << e->getName() << " , " << e->getElementInfo()->getType() << std::endl;
+
+    std::cout<<"Terminals von C2:"<<std::endl;
+    std::vector<const TerminalInfo*> inTerm =  graphHandler->getElement(1)->getElementInfo()->getInputTerminalInfos();
+    std::vector<const TerminalInfo*> outTerm =  graphHandler->getElement(1)->getElementInfo()->getOutputTerminalInfos();
+    for (const TerminalInfo* t:inTerm)
+        std::cout << t->getName() << " , " << t->getType() << std::endl;
+    for (const TerminalInfo* t:outTerm)
+        std::cout << t->getName() << " , " << t->getType() << std::endl;
 }
 
 
